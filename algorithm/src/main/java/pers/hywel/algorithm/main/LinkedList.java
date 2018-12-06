d4105dfeff555d866e09eec764b54c19ee465d76
hywel <1051892427@qq.com>
2018-12-06 16:58:16 +0800

    rm redundent file

---
+++ algorithm/src/main/java/pers/hywel/algorithm/main/LinkedList.java
@@ -0,0 +1,10 @@
+package org.hywel.algorithm.main;
+
+import org.hywel.algorithm.linkList.SingleLinkedList;
+
+public class LinkedList {
+    public static void main(String[] args) {
+        SingleLinkedList obj = new SingleLinkedList();
+
+    }
+}
